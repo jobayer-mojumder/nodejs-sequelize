4042b5cb500f09f4b87553e0250b30d44608f3b2
jobayermojumder <jobayer.pro@gmail.com>
2020-07-07 01:57:26 +0200

    app.js file created

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    console.log('Welcome to the application')
+    res.status(200).json({
+        message: "Welcome to the application"
+    });
+})
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log("server up and running on PORT :", port);
+});
